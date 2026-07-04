93593569fc91ec717715ee53683b8ad3ad22a969
satyajit <satyajit.1418@gmail.com>
2026-07-04 22:50:36 +0530

    Revert "fix: rename menifests to manifests"
    This reverts commit 7a145a4951c26e0c1286247b75da2d12ddbef8a3.

--- terraform/backend/output.tf
+++
@@ -1,8 +0,0 @@
-output "arn" {
-  description = "The ARN of the S3 bucket"
-  value = aws_s3_bucket.terraform_state.arn
-}
-output "lock_table_name" {
-  description = "The name of the DynamoDB table used for state locking"
-  value = aws_dynamodb_table.terraform_state_lock.name
-}
